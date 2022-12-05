0ff384c671758473293e5ea93dda16e26a8a3d9b
thud aneknual <monhomes@hotmail.com>
2022-12-05 15:16:47 +0700

    init

---
+++ grid-design-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LeftNavBarComponent } from './LeftNavBar/left-nav-bar/left-nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LeftNavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
